56c8a62bbebff37d8583f4dee931687fbcc2de1f
Sam Kumar <samkumar99@gmail.com>
2016-05-05 22:51:06 -0700

    Added another test for Rebaser

--- rebaser_test.go
+++ rebaser_test.go
@@ -7,8 +7,9 @@ import (
 	btrdb "github.com/SoftwareDefinedBuildings/btrdb-go"
 )
 
-const SPACING_INT64 int64 = 1000000000 / 120
+const NANO int64 = 1000000000
 const FREQ int64 = 120
+const SPACING_INT64 int64 = NANO / FREQ
 
 func sliceToChan(s []btrdb.StandardValue) chan btrdb.StandardValue {
 	var rv chan btrdb.StandardValue = make(chan btrdb.StandardValue)
@@ -35,9 +36,9 @@ func floatEquals(x float64, y float64) bool {
 	return math.Abs(x - y) <= 1e-10 * math.Max(math.Abs(x), math.Abs(y))
 }
 
-func TestFullyAligned(t *testing.T) {
-	var s []btrdb.StandardValue = make([]btrdb.StandardValue, 0, 360)
-	for second := int64(0); second < 3; second++ {
+func makeAligned(ssec, esec int64) []btrdb.StandardValue {
+	var s []btrdb.StandardValue = make([]btrdb.StandardValue, 0, (esec - ssec) * 120)
+	for second := int64(ssec); second < esec; second++ {
 		for i := int64(0); i < 120; i++ {
 			t := (second * 1000000000) + (i * SPACING_INT64)
 			v := float64(i)
@@ -45,11 +46,17 @@ func TestFullyAligned(t *testing.T) {
 			s = append(s, sv)
 		}
 	}
+	return s
+}
+
+func TestFullyAligned(t *testing.T) {
+	s := makeAligned(0, 3)
+	
 	
 	var c chan btrdb.StandardValue = sliceToChan(s)
 	var pnr Rebaser = RebasePadSnap(FREQ)
 	
-	var oc chan btrdb.StandardValue = pnr.Process(0, 3000000000, c)
+	var oc chan btrdb.StandardValue = pnr.Process(0 * NANO, 3 * NANO, c)
 	var os []btrdb.StandardValue = chanToSlice(oc)
 	
 	if len(os) != len(s) {
@@ -59,6 +66,46 @@ func TestFullyAligned(t *testing.T) {
 	for i := range os {
 		if os[i].Time != s[i].Time || !floatEquals(os[i].Value, s[i].Value) {
 			t.Logf("Got (%d, %f) at entry %d; expected (%d, %f)", os[i].Time, os[i].Value, i, s[i].Time, s[i].Value)
+			t.Fail()
+		}
+	}
+}
+
+func TestExtraSpace(t *testing.T) {
+	s := makeAligned(2, 5)
+	
+	var c chan btrdb.StandardValue = sliceToChan(s)
+	var pnr Rebaser = RebasePadSnap(FREQ)
+	
+	var oc chan btrdb.StandardValue = pnr.Process(1 * NANO, 6 * NANO, c)
+	var os []btrdb.StandardValue = chanToSlice(oc)
+	
+	var explen int64 = int64(len(s)) + 2 * FREQ
+	if int64(len(os)) != explen {
+		t.Fatalf("Output has %d points (expected %d)", len(os), explen)
+	}
+	
+	var i int64
+	for i = 0; i < FREQ; i++ {
+		expt := (1 * NANO) + i * SPACING_INT64
+		if os[i].Time != expt || !math.IsNaN(os[i].Value) {
+			t.Logf("Got (%d, %f) at entry %d; expected (%d, NaN)", os[i].Time, os[i].Value, i, expt)
+			t.Fail()
+		}
+	}
+	for i = 0; i < FREQ; i++ {
+		expt := (5 * NANO) + i * SPACING_INT64
+		j := i + 4 * FREQ
+		if os[j].Time != expt || !math.IsNaN(os[j].Value) {
+			t.Logf("Got (%d, %f) at entry %d; expected (%d, NaN)", os[j].Time, os[j].Value, j, expt)
+			t.Fail()
+		}
+	}
+	
+	for j := range s {
+		i := int64(j) + FREQ
+		if os[i].Time != s[j].Time || !floatEquals(os[i].Value, s[j].Value) {
+			t.Logf("Got (%d, %f) at entry %d; expected (%d, %f)", os[i].Time, os[i].Value, j, s[j].Time, s[j].Value)
 			t.Fail()
 		}
 	}
