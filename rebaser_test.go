5a6c99745c480a47290fcfdd5cc8924316ea4329
Sam Kumar <samkumar99@gmail.com>
2016-05-05 21:48:20 -0700

    Add test for Rebaser

--- rebaser_test.go
+++ rebaser_test.go
@@ -1,20 +1,20 @@
 package distil
 
 import (
+	"math"
 	"testing"
 
 	btrdb "github.com/SoftwareDefinedBuildings/btrdb-go"
 )
 
-const SPACING float64 = 1e9 / 120.0
-const SPACING_INT64 int64 = int64(SPACING)
-const FREQ float64 = 1.0 / SPACING
+const SPACING_INT64 int64 = 1000000000 / 120
+const FREQ int64 = 120
 
 func sliceToChan(s []btrdb.StandardValue) chan btrdb.StandardValue {
 	var rv chan btrdb.StandardValue = make(chan btrdb.StandardValue)
 	
 	go func () {
-		for sv := range s {
+		for _, sv := range s {
 			rv <- sv
 		}
 		close(rv)
@@ -24,7 +24,7 @@ func sliceToChan(s []btrdb.StandardValue) chan btrdb.StandardValue {
 }
 
 func chanToSlice(svs chan btrdb.StandardValue) []btrdb.StandardValue {
-	var rv = make([]btrdb.StandardValue)
+	var rv = make([]btrdb.StandardValue, 0, 8)
 	for sv := range svs {
 		rv = append(rv, sv)
 	}
@@ -32,12 +32,12 @@ func chanToSlice(svs chan btrdb.StandardValue) []btrdb.StandardValue {
 }
 
 func floatEquals(x float64, y float64) bool {
-	return math.Abs(x - y) < 1e-10 * math.Max(math.Abs(x), math.Abs(y))
+	return math.Abs(x - y) <= 1e-10 * math.Max(math.Abs(x), math.Abs(y))
 }
 
 func TestFullyAligned(t *testing.T) {
-	var s []btrdb.StandardValue = make([]btrdb.StandardValue, 360)
-	for second := 0; second < 3; second++ {
+	var s []btrdb.StandardValue = make([]btrdb.StandardValue, 0, 360)
+	for second := int64(0); second < 3; second++ {
 		for i := int64(0); i < 120; i++ {
 			t := (second * 1000000000) + (i * SPACING_INT64)
 			v := float64(i)
@@ -47,7 +47,7 @@ func TestFullyAligned(t *testing.T) {
 	}
 	
 	var c chan btrdb.StandardValue = sliceToChan(s)
-	var r pnr = RebasePadSnap(freq)
+	var pnr Rebaser = RebasePadSnap(FREQ)
 	
 	var oc chan btrdb.StandardValue = pnr.Process(0, 3000000000, c)
 	var os []btrdb.StandardValue = chanToSlice(oc)
@@ -56,9 +56,9 @@ func TestFullyAligned(t *testing.T) {
 		t.Fatalf("Output has %d points (expected %d)", len(os), len(s))
 	}
 	
-	for i, osv := range os {
+	for i := range os {
 		if os[i].Time != s[i].Time || !floatEquals(os[i].Value, s[i].Value) {
-			t.Logf("Got (%d, %f) at entry %d; expected (%d, %f)", os[i].Time, s[i].Time, i, os[i].Value, s[i].Value)
+			t.Logf("Got (%d, %f) at entry %d; expected (%d, %f)", os[i].Time, os[i].Value, i, s[i].Time, s[i].Value)
 			t.Fail()
 		}
 	}
